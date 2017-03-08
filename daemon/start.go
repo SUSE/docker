bec7fac20974cadb313eaf23ef26dc828ee290aa
Aleksa Sarai <asarai@suse.de>
2017-03-08 11:43:29 +1100

    SECRETS: SUSE: implement SUSE container secrets
    This allows for us to pass in host credentials to a container, allowing
    for SUSEConnect to work with containers.
    
    THIS PATCH IS NOT TO BE UPSTREAMED, DUE TO THE FACT THAT IT IS
    SUSE-SPECIFIC, AND UPSTREAM DOES NOT APPROVE OF THIS CONCEPT BECAUSE IT
    MAKES BUILDS NOT ENTIRELY REPRODUCIBLE.
    
    SUSE-Bugs: bsc#1065609 bsc#1057743 bsc#1055676 bsc#1030702
    Signed-off-by: Aleksa Sarai <asarai@suse.de>

--- daemon/start.go
+++ daemon/start.go
@@ -123,6 +123,11 @@ func (daemon *Daemon) containerStart(ctx context.Context, daemonCfg *configStore
 		return err
 	}
 
+	// SUSE:secrets -- inject the SUSE secret store
+	if err := daemon.injectSuseSecretStore(container); err != nil {
+		return err
+	}
+
 	m, cleanup, err := daemon.setupMounts(ctx, container)
 	if err != nil {
 		return err
