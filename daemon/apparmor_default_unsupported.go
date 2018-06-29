c19fad9e09248bf390fe9b2cd38f351104f186b8
Aleksa Sarai <asarai@suse.de>
2018-06-29 17:59:30 +1000

    bsc1073877: apparmor: clobber docker-default profile on start
    In the process of making docker-default reloading far less expensive,
    567ef8e7858c ("daemon: switch to 'ensure' workflow for AppArmor
    profiles") mistakenly made the initial profile load at dockerd start-up
    lazy. As a result, if you have a running Docker daemon and upgrade it to
    a new one with an updated AppArmor profile the new profile will not take
    effect (because the old one is still loaded). The fix for this is quite
    trivial, and just requires us to clobber the profile on start-up.
    
    Fixes: 567ef8e7858c ("daemon: switch to 'ensure' workflow for AppArmor profiles")
    SUSE-Bugs: bsc#1099277
    Signed-off-by: Aleksa Sarai <asarai@suse.de>

--- daemon/apparmor_default_unsupported.go
+++ daemon/apparmor_default_unsupported.go
@@ -3,6 +3,10 @@
 
 package daemon // import "github.com/docker/docker/daemon"
 
+func clobberDefaultAppArmorProfile() error {
+	return nil
+}
+
 func ensureDefaultAppArmorProfile() error {
 	return nil
 }
