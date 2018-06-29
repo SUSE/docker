bedb46f195f9622f750a12d42b5d74bcc50923b8
Aleksa Sarai <asarai@suse.de>
2018-06-29 17:59:30 +1000

    bsc1073877: apparmor: clobber docker-default profile on start
    In the process of making docker-default reloading far less expensive,
    567ef8e7858c ("daemon: switch to 'ensure' workflow for AppArmor
    profiles") mistakenly made the initial profile load at dockerd start-up
    lazy. As a result, if you have a running Docker daemon and upgrade it to
    a new one with an updated AppArmor profile the new profile will not take
    effect (because the old one is still loaded). The fix for this is quite
    trivial, and just requires us to clobber the profile on start-up.
    
    Fixes: 567ef8e7858c ("daemon: switch to 'ensure' workflow for AppArmor profiles")
    SUSE-Bugs: bsc#1099277
    Signed-off-by: Aleksa Sarai <asarai@suse.de>

--- daemon/apparmor_default.go
+++ daemon/apparmor_default.go
@@ -24,6 +24,15 @@ func DefaultApparmorProfile() string {
 	return ""
 }
 
+func clobberDefaultAppArmorProfile() error {
+	if apparmor.HostSupports() {
+		if err := aaprofile.InstallDefault(defaultAppArmorProfile); err != nil {
+			return fmt.Errorf("AppArmor enabled on system but the %s profile could not be loaded: %s", defaultAppArmorProfile, err)
+		}
+	}
+	return nil
+}
+
 func ensureDefaultAppArmorProfile() error {
 	if apparmor.HostSupports() {
 		loaded, err := aaprofile.IsLoaded(defaultAppArmorProfile)
@@ -37,10 +46,7 @@ func ensureDefaultAppArmorProfile() error {
 		}
 
 		// Load the profile.
-		if err := aaprofile.InstallDefault(defaultAppArmorProfile); err != nil {
-			return fmt.Errorf("AppArmor enabled on system but the %s profile could not be loaded: %s", defaultAppArmorProfile, err)
-		}
+		return clobberDefaultAppArmorProfile()
 	}
-
 	return nil
 }
