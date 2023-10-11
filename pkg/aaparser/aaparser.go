e4c2b3e6b168e815ec7248aea696afe807153cb6
Aleksa Sarai <asarai@suse.de>
2023-10-11 21:19:12 +1100

    SLE12: revert "apparmor: remove version-conditionals from template"
    This reverts the following commits:
    
     * 7008a514493a ("profiles/apparmor: remove version-conditional constraints (< 2.8.96)")
     * 2e19a4d56bf2 ("contrib/apparmor: remove version-conditionals (< 2.9) from template")
     * d169a5730649 ("contrib/apparmor: remove remaining version-conditionals (< 2.9) from template")
     * ecaab085db4b ("profiles/apparmor: remove use of aaparser.GetVersion()")
     * e3e715666f95 ("pkg/aaparser: deprecate GetVersion, as it's no longer used")
    
    These version conditionals are still required on SLE 12, where our
    apparmor_parser version is quite old.
    
    Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

--- pkg/aaparser/aaparser.go
+++ pkg/aaparser/aaparser.go
@@ -13,8 +13,6 @@ const (
 )
 
 // GetVersion returns the major and minor version of apparmor_parser.
-//
-// Deprecated: no longer used, and will be removed in the next release.
 func GetVersion() (int, error) {
 	output, err := cmd("", "--version")
 	if err != nil {
