35f1693d5585b742a6749964c9bd05859c33f64b
Aleksa Sarai <asarai@suse.de>
2023-10-11 21:19:12 +1100

    SLE12: revert "apparmor: remove version-conditionals from template"
    This reverts the following commits:
    
     * 7008a514493a ("profiles/apparmor: remove version-conditional constraints (< 2.8.96)")
     * 2e19a4d56bf2 ("contrib/apparmor: remove version-conditionals (< 2.9) from template")
     * d169a5730649 ("contrib/apparmor: remove remaining version-conditionals (< 2.9) from template")
     * ecaab085db4b ("profiles/apparmor: remove use of aaparser.GetVersion()")
     * e3e715666f95 ("pkg/aaparser: deprecate GetVersion, as it's no longer used")
    
    These version conditionals are still required on SLE 12, where our
    apparmor_parser version is quite old.
    
    Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

--- profiles/apparmor/apparmor.go
+++ profiles/apparmor/apparmor.go
@@ -14,8 +14,10 @@ import (
 	"github.com/docker/docker/pkg/aaparser"
 )
 
-// profileDirectory is the file store for apparmor profiles and macros.
-const profileDirectory = "/etc/apparmor.d"
+var (
+	// profileDirectory is the file store for apparmor profiles and macros.
+	profileDirectory = "/etc/apparmor.d"
+)
 
 // profileData holds information about the given profile for generation.
 type profileData struct {
@@ -27,6 +29,8 @@ type profileData struct {
 	Imports []string
 	// InnerImports defines the apparmor functions to import in the profile.
 	InnerImports []string
+	// Version is the {major, minor, patch} version of apparmor_parser as a single number.
+	Version int
 }
 
 // generateDefault creates an apparmor profile from ProfileData.
@@ -45,6 +49,12 @@ func (p *profileData) generateDefault(out io.Writer) error {
 	if macroExists("abstractions/base") {
 		p.InnerImports = append(p.InnerImports, "#include <abstractions/base>")
 	}
+
+	ver, err := aaparser.GetVersion()
+	if err != nil {
+		return err
+	}
+	p.Version = ver
 
 	return compiled.Execute(out, p)
 }
