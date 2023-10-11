7bd32fa91ed29b32d42991304b9a55a1f7db2ece
Aleksa Sarai <asarai@suse.de>
2023-10-11 21:19:12 +1100

    SLE12: revert "apparmor: remove version-conditionals from template"
    This reverts the following commits:
    
     * 7008a514493a ("profiles/apparmor: remove version-conditional constraints (< 2.8.96)")
     * 2e19a4d56bf2 ("contrib/apparmor: remove version-conditionals (< 2.9) from template")
     * d169a5730649 ("contrib/apparmor: remove remaining version-conditionals (< 2.9) from template")
     * ecaab085db4b ("profiles/apparmor: remove use of aaparser.GetVersion()")
     * e3e715666f95 ("pkg/aaparser: deprecate GetVersion, as it's no longer used")
    
    These version conditionals are still required on SLE 12, where our
    apparmor_parser version is quite old.
    
    Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

--- vendor/github.com/moby/profiles/apparmor/template.go
+++ vendor/github.com/moby/profiles/apparmor/template.go
@@ -22,6 +22,7 @@ profile {{.Name}} flags=(attach_disconnected,mediate_deleted) {
   capability,
   file,
   umount,
+{{if ge .Version 208096}}
   # Host (privileged) processes may send signals to container processes.
   signal (receive) peer=unconfined,
   # runc may send signals to container processes (for "docker stop").
@@ -32,6 +33,7 @@ profile {{.Name}} flags=(attach_disconnected,mediate_deleted) {
   signal (receive) peer={{.DaemonProfile}},
   # Container processes may send signals amongst themselves.
   signal (send,receive) peer={{.Name}},
+{{end}}
 
   deny @{PROC}/* w,   # deny write for all files directly in /proc (not in a subdir)
   # deny write to files not in /proc/<number>/** or /proc/sys/**
@@ -52,7 +54,9 @@ profile {{.Name}} flags=(attach_disconnected,mediate_deleted) {
   deny /sys/devices/virtual/powercap/** rwklx,
   deny /sys/kernel/security/** rwklx,
 
+{{if ge .Version 208095}}
   # suppress ptrace denials when using 'docker ps' or using 'ps' inside a container
   ptrace (trace,read,tracedby,readby) peer={{.Name}},
+{{end}}
 }
 `
